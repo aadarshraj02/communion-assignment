c90c0a0853eaf489a1735b1ca16b75b268a4ace7
adarsh02 <aadarshraazz007@gmail.com>
2025-03-09 09:33:52 +0530

    setup routes for home page and evenyt pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,15 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Events from "./pages/Events";
+
 const App = () => {
-  return <div className="">App</div>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/events" element={<Events />} />
+      </Routes>
+    </Router>
+  );
 };
 export default App;
