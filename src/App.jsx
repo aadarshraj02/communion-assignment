3477219be016dea2f927366ab6a9e6d707225363
adarsh02 <aadarshraazz007@gmail.com>
2025-03-09 17:30:06 +0530

    imported and added header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Events from "./pages/Events";
+import Header from "./components/Header";
 
 const App = () => {
   return (
     <Router>
       <Routes>
+        <Header />
         <Route path="/" element={<Home />} />
         <Route path="/events" element={<Events />} />
       </Routes>
