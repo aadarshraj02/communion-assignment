cf14e82f2ecdb3c4551dc6940a2208760734c4b6
adarsh02 <aadarshraazz007@gmail.com>
2025-03-11 15:25:13 +0530

    added route for about

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Events from "./pages/Events";
 import Header from "./components/Header";
+import About from "./pages/About";
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/events" element={<Events />} />
+          <Route path="/about" element={<About />} />
         </Routes>
       </main>
     </Router>
