a68e456c3713eaede83d79570cf520c2ff45db57
adarsh02 <aadarshraazz007@gmail.com>
2025-03-11 12:45:26 +0530

    added dummy data

---
+++ src/data/events.js
@@ -0,0 +1,75 @@
+const events = [
+  {
+    id: 1,
+    title: "Sunday Worship Gathering",
+    date: "2025-03-16",
+    location: "Lotus Temple, Delhi",
+    description:
+      "Join us for a peaceful Sunday worship with uplifting music and inspiring messages.",
+    category: "Religious",
+  },
+  {
+    id: 2,
+    title: "Community Service Day",
+    date: "2025-03-18",
+    location: "Andheri, Mumbai",
+    description:
+      "Volunteer with us to give back to the community through various service activities.",
+    category: "Charity",
+  },
+  {
+    id: 3,
+    title: "Faith & Leadership Seminar",
+    date: "2025-03-20",
+    location: "Chandigarh University, Chandigarh",
+    description:
+      "A seminar on faith-driven leadership and making an impact in your community.",
+    category: "Social",
+  },
+  {
+    id: 4,
+    title: "Youth Fellowship Night",
+    date: "2025-03-22",
+    location: "Gandhi Maidan, Patna",
+    description:
+      "An evening for young people to connect, share stories, and grow in faith together.",
+    category: "Social",
+  },
+  {
+    id: 5,
+    title: "Gospel Music Festival",
+    date: "2025-03-25",
+    location: "Music Hall, Goa",
+    description:
+      "Enjoy a night of live gospel music from talented artists and choirs.",
+    category: "Social",
+  },
+  {
+    id: 6,
+    title: "Story Study Group",
+    date: "2025-03-28",
+    location: "Library, J&K",
+    description:
+      "Deepen your understanding of the Stories in a friendly discussion group.",
+    category: "Charity",
+  },
+  {
+    id: 7,
+    title: "Rameshwaram Visit",
+    date: "2025-04-02",
+    location: "Rameshwaram, Tamil Nadu",
+    description: "A weekend Visit to Rameshwaram sea sighting and spiritual.",
+    category: "Religious",
+  },
+  {
+    id: 8,
+    title: "Movie Release Party",
+    date: "2025-04-05",
+    location: "Elante Mall, Mumbai",
+    description:
+      "Watch Party of Upcoming movie release watch and enjoy the movie with friends.",
+    category: "Social",
+  },
+];
+
+export default events;
